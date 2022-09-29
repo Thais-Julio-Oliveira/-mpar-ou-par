c150fb416104b957f0ed7050f01b76dd74a65a5b
Thais <thathafusionmother@gmail.com>
2022-09-28 23:18:19 -0300

    Primeiro commit

---
+++ imparoupar.py
@@ -0,0 +1,15 @@
+número_inteiro = input ('Informe um número inteiro ')
+
+if número_inteiro.isdigit():
+    número_inteiro = int(número_inteiro)
+
+    if número_inteiro % 2 == 0:
+        print(f'{número_inteiro} é par')
+    else:
+
+        print(f'{número_inteiro} é ímpar')
+
+
+
+
+
